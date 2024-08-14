7ec95e058af01d8ed02e5dd4c9a6441258c32c79
emmaimade <emmaimade14@gmail.com>
2024-08-14 01:00:58 +0100

    feat: implement post endpoint

--- routes/url.js
+++ routes/url.js
@@ -6,8 +6,38 @@ import URL from "../models/URL.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render("index");
+router.get("/", async (req, res) => {
+    const baseUrl = req.get("host");
+
+    const urls = await URL.find();
+    res.render("index", {shortUrl: urls, baseUrl});
+});
+
+router.post("/shorten", async (req, res) => {
+    try {
+        const { fullUrl } = req.body;
+
+        // checks if url is valid
+        if (!isUrl(fullUrl)) {
+            res.render("index", { error: "Please enter a valid URL" });
+        }
+
+        // checks if url is exist
+        const url = await URL.findOne({ fullUrl });
+        if (url) {
+            return res.render("index", {shortUrl: url.shortUrl});
+        }
+
+        const shortUrl = new URL({
+            fullUrl
+        });
+
+        await shortUrl.save();
+        res.redirect("/");
+        
+    } catch (error) {
+        console.log({error: error.message});        
+    }
 });
 
 
