e462a00b596c01099f395347299411c5046c09c3
emmaimade <emmaimade14@gmail.com>
2025-04-10 23:41:49 +0100

    Modify User Interface

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 const dbURI = process.env.DB_URI;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 mongoose.connect(dbURI)
